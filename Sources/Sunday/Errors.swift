22fe1526b985e96e52e71a35385deb4f31369594
Kevin Wooten <kevin@outfoxx.io>
2020-08-20 19:21:23 -0700

    task: remove unused errors & reasons

--- Sources/Sunday/Errors.swift
+++ Sources/Sunday/Errors.swift
@@ -11,11 +11,6 @@
 import Foundation
 
 
-public enum ParameterEncodingFailureReason {
-  case unsupportedContentType(MediaType)
-  case serializationFailed(contentType: MediaType, error: Error?)
-}
-
 public enum RequestEncodingFailureReason {
   case noSupportedContentType([MediaType])
   case unsupportedContentType(MediaType)
@@ -26,7 +21,6 @@ public enum ResponseDecodingFailureReason {
   case invalidContentType(String)
   case unsupportedContentType(MediaType)
   case inputDataNilOrZeroLength
-  case stringDecodingFailed(encoding: String.Encoding)
   case serializationFailed(contentType: MediaType, error: Error?)
   case missingValue
 }
@@ -36,15 +30,10 @@ public enum ResponseValidationFailureReason {
 }
 
 public enum SundayError: Swift.Error {
-  case parameterEncodingFailed(reason: ParameterEncodingFailureReason)
   case requestEncodingFailed(reason: RequestEncodingFailureReason)
   case responseDecodingFailed(reason: ResponseDecodingFailureReason)
   case responseValidationFailed(reason: ResponseValidationFailureReason)
   case unexpectedEmptyResponse
-  case unexpectedDataResponse
-  case notFound
   case invalidURL(URLComponents? = nil)
-  case invalidTemplate
-  case unknownNetworkError
   case invalidHTTPResponse
 }
