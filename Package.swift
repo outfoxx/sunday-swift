fa085f7d4e95598a461f3440d7dc57a890813975
Kevin Wooten <kevin@outfoxx.io>
2021-05-16 02:52:49 -0700

    build: add CombineExpectations as a test dependency

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
     .package(url: "https://github.com/outfoxx/PotentCodables.git", from: "1.7.1"),
     .package(url: "https://github.com/outfoxx/OSLogTrace.git", from: "1.1.1"),
     .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
-    .package(url: "https://github.com/SwiftScream/URITemplate.git", from: "2.1.0")
+    .package(url: "https://github.com/SwiftScream/URITemplate.git", from: "2.1.0"),
+    .package(url: "https://github.com/groue/CombineExpectations.git", from: "0.7.0")
   ],
   targets: [
     .target(
@@ -47,6 +48,7 @@ let package = Package(
       dependencies: [
         "Sunday",
         "SundayServer",
+        "CombineExpectations"
       ]
     ),
   ]
